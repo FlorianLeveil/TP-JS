cd40ab1905961fc346544e48b1cffbfb14a69510
FlorianLeveil <FlorianLeveil>
2020-01-28 09:50:14 +0100

    creation du dossier

---
+++ Leveil-JSLand/caissier.js
@@ -0,0 +1,15 @@
+import Personne from "./personne";
+
+export default class Client extends Personne{
+    historique = [0,"0"]
+
+    constructor(prenom, nom, budget) {
+        super(nom, prenom);
+        this.budget = budget;
+    };
+
+    function int payer(prix) {
+
+        return budget
+    }
+}
